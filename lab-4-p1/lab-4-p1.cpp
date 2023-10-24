43133f39adcd361480fe6e963985716250174c96
ZarwalD <mdogminec@gmail.com>
2023-10-24 10:18:26 +0700

    Добавьте файлы проекта.

---
+++ lab-4-p1/lab-4-p1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <clocale>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int i;
+    std::cout << "Введите число: ";
+    std::cin >> i;
+    if (i < 0)
+    {
+        std::cout << "Введите корректное число" << std::endl;
+        return 1;
+    }
+    for (int j = 0; j < i; j++)
+    {
+        int k;
+        std::cin >> k;
+        if (k < 0)
+        {
+            std::cout << "FALSE";
+            return 1;
+        }
+    }
+    std::cout << "TRUE";
+}
